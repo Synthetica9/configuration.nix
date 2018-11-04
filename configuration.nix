0b052e2c641e4543562733e036d56822c28e8446
Patrick Hilhorst <git@hilhorst.be>
2018-11-04 18:35:51 +0100

    Add forgotten semicolons

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,5 @@
 let
-    hostname = ...
-    url = "https://github.com/synthetica9/configuration.nix/archive/master.tar.gz"
+    hostname = ...;
+    url = "https://github.com/synthetica9/configuration.nix/archive/master.tar.gz";
 in
     (import (builtins.fetchTarball url)) hostname ./hardware-configuration.nix
