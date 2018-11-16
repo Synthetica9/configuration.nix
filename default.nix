18f96b701a6d795fad1a4f803b2e5a1681c3ac51
Patrick Hilhorst <git@hilhorst.be>
2018-11-16 15:15:11 +0100

    Revert "Add GHCRTS -N flag to env"
    This reverts commit 57275a7f4afceebefd38cb68b50d649f07c4b052.

--- default.nix
+++ default.nix
@@ -12,11 +12,9 @@ let
   machine = {
     AquaRing = {
       arch = "broadwell";
-      cores = 4;
     };
     Will-O-Wisp = {
       arch = "sandybridge";
-      cores = 4;
     };
     # Not a real machine, but used in local.nix
     testing = {};
@@ -481,7 +479,6 @@ in {
       "XCURSOR_THEME" = "numix";
       "EDITOR" = "code --wait";
       "VISUAL" = "code --wait";
-      "GHCRTS" = "-N${toString machine.cores or 1}";
       # "XDG_DATA_DIRS" = builtins.concatStringsSep ":" [
       #   "/run/opengl-driver/share"
       #   "/run/opengl-driver-32/share"
