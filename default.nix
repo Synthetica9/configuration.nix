fb35b342f3bd2d69f1345283672b92592ffad63e
Patrick Hilhorst <git@hilhorst.be>
2018-11-27 02:12:07 +0100

    Add nix-linter cachix cache

--- default.nix
+++ default.nix
@@ -649,10 +649,12 @@ in {
       "https://cache.nixos.org/"
       "https://hie-nix.cachix.org"
       "https://hnix.cachix.org"
+      "https://nix-linter.cachix.org"
     ];
     binaryCachePublicKeys = [
       "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
       "hnix.cachix.org-1:8MflOlogfd6Y94rD0cjHsmfK0qIF8F5dPz4TSY7qSdU="
+      "nix-linter.cachix.org-1:BdTne5LEHQfIoJh4RsoVdgvqfObpyHO5L0SCjXFShlE"
     ];
     trustedUsers = [ "root" "synthetica" ];
     gc = {
