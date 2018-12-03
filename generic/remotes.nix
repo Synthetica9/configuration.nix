bd8d15678fb2667b57b0c15e0d8517c3a24f8954
Patrick Hilhorst <git@hilhorst.be>
2018-12-03 14:21:25 +0100

    Minor refactor in remotes.nix, make it slightly more streamlined

--- generic/remotes.nix
+++ generic/remotes.nix
@@ -11,17 +11,17 @@ let
     upstream = "nixos";
     devel = "synthetica9";
   };
-in
+in with remotes;
 {
   nixpkgs.config.packageOverrides = {
     hieRemote = import (builtins.fetchTarball "https://github.com/domenkozar/hie-nix/archive/master.tar.gz") {};
 
     master = builtins.trace
       "Warning: master is impure. Using this might have unexpected consequences!"
-      (remotes.upstream "master");
-    haskell-fix     = remotes.upstream "69fa2d6bfba";
-    libreoffice-fix = remotes.upstream "81f5c2698a8";
-    steam-fix = remotes.devel "steamRuntimeRefactor";
-    konsole-fix = remotes.devel "konsole-keyboard-fix";
+      (upstream "master");
+    haskell-fix     = upstream "69fa2d6bfba";
+    libreoffice-fix = upstream "81f5c2698a8";
+    steam-fix = devel "steamRuntimeRefactor";
+    konsole-fix = devel "konsole-keyboard-fix";
   };
 }
