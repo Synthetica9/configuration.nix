2a6144f95e05c1f88e7c2cadea64d6a5051e655b
Patrick Hilhorst <git@hilhorst.be>
2018-12-11 23:23:09 +0100

    Trace value on channel usage

--- channels/default.nix
+++ channels/default.nix
@@ -4,11 +4,16 @@ let
   defs = lib.importJSON ./defs.json;
   channels' = lib.listToAttrs (map (val: {
     name = val.name;
-    value = builtins.fetchTarball "https://github.com/nixos/Nixpkgs/archive/${val.commit.sha}.tar.gz";
+    value = builtins.fetchTarball {
+      url = "https://github.com/nixos/Nixpkgs/archive/${val.commit.sha}.tar.gz";
+      name = lib.debug.traceVal "${val.name}-${val.commit.sha}";
+    };
   }) defs);
-  channels = lib.mapAttrs (key: val: import val {
-    config = config.nixpkgs.config;
-  }) channels';
+  channels = lib.mapAttrs (key: val:
+    import val {
+      config = config.nixpkgs.config;
+    }
+  ) channels';
 in
 {
   nixpkgs.config.packageOverrides = { inherit channels channels'; } // channels;
